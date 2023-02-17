437440c252381584569525f11fa0c7da67ebddf8
facecontrol34 <99085465+facecontrol34@users.noreply.github.com>
2023-02-17 21:09:12 +0300

    Add files via upload

---
+++ app1/app1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine("Введите количество элементов массива массива: ");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[lenght];
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = rnd.Next(-11, 11);
+                Console.Write(array[i] + ", ");
+
+            }
+
+            for (int i = 1; i < lenght; i++)
+            {
+                int k = array[i];
+                int j = i - 1;
+                while (j >= 0 && k < array[j])
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = k;
+            }
+            Console.WriteLine("\nОтсортированный массив: ");
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write(array[i] + ", ");
+            }
+        }
+    }
+}
